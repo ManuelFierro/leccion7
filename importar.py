5f59415416ad6c948b3474b5c0471fb642b4df5c
Mafc88 <mafc88@gmail.com>
2020-03-12 11:40:43 -0600

    intereactuando con sql desde phyton

--- importar.py
+++ importar.py
@@ -7,6 +7,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 engine = create_engine(os.getenv('DATABASE_URL'))
 db = scoped_session(sessionmaker(bind=engine))
 
+# listar vuelos
+
 
 def main():
     fo = open("vuelos.csv")
@@ -16,6 +18,16 @@ def main():
                    "origen": origen, "destino": destino, "duracion": duracion})
         print(f"agregados vuelos desde {origen} a {destino} durando {duracion} minutos.")
     db.commit()
+# solicitar que el usuario elija un vuelo
+    vuelos_id = int(input("\n Vuelo ID: "))
+    vuelos = db.execute("SELECT origen, destino, duracion FROM vuelos WHERE id = :id",
+                        {"id": vuelos_id}).fetchone()
+    print(
+        f"El vuelo elejido es el numero: {vuelos_id}, de {vuelos.origen} a {vuelos.destino} con una duracion de {vuelos.duracion} minutos")
+    # asegurar que el vuelo sea valido
+    if vuelos is None:
+        print("ERROR EL VUELO NO EXISTE")
+        return
 
 
 if __name__ == "__main__":
