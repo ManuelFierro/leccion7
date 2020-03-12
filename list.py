1e3e5852db52dd5bf09edf5b69de7bb0de36125e
Mafc88 <mafc88@gmail.com>
2020-03-12 08:25:42 -0600

    leccion 7 list.py

--- list.py
+++ list.py
@@ -4,14 +4,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
-engine = create_engine('postgres://postgres:admin@localhost:5432/aerolinea')
+# sentencia carga motor bd directo
+motor = create_engine("postgres://postgres:admin@localhost:5432/postgres")
+
+
+# llama la variable de entorno, debe tener comillas simples
+engine = create_engine(os.getenv('DATABASE_URL'))
 db = scoped_session(sessionmaker(bind=engine))
 
 
 def main():
-    vuelos = db.execute("SELECT origen, destino, duracion FROM vuelos").fetchall()
-    for vuelo in vuelos:
-        print("{vuelo.origen} a {vuelo.destino}, {vuelo.duracion} minutos.")
+    vls = db.execute("SELECT origen, destino, duracion FROM vuelos").fetchall()
+    for vuelo in vls:
+        print(f"{vuelo.origen} a {vuelo.destino}, {vuelo.duracion} minutos.")
 
 
 if __name__ == "__main__":
